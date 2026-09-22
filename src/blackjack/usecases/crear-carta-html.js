97985e51cd6b089922e821232955c4e2e4de691c
Marroxz <marroxbox@hotmail.com>
2026-09-21 19:40:36 -0700

    First commit

---
+++ src/blackjack/usecases/crear-carta-html.js
@@ -0,0 +1,15 @@
+
+/**
+ * Esta funcion crea la imagen de la carta
+ * @param {String} carta carta
+ * @returns {HTMLImageElement} Elemento HTML de retorno
+ */
+export const crearCartaHTML = (carta) => {
+    if(!carta) throw new Error ('La carta es un argumento obligatorio');
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`; //3H, JD
+    imgCarta.classList.add('carta');
+
+    return imgCarta;
+}
---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export { crearDeck } from './crear-deck'; // importacion independiente
+export { valorCarta } from './valor-carta';
+export {pedirCarta} from './pedir-carta'; // importacion independiente de pedir carta.
+export {turnoComputadora} from './turno-computadora';
+export {crearCartaHTML} from './crear-carta-html'
