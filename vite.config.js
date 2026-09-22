54b4eed74abbc0ae9d14b54d0f259af130db204a
Marroxz <marroxbox@hotmail.com>
2026-09-22 02:48:43 -0700

    Vite-config localhost added

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import {defineConfig} from 'vite'
 
+// function defineConfig(config){
+//     return config.base
+// }
+
 export default defineConfig({
+    server:{
+        port:3000
+    },
     base: '/js-vite-blackjack/',
 });
